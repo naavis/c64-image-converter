bd2b560dff9393a8d10b4b19dc612a00f4bdab62
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2016-07-20 16:00:55 +0300

    Finish color reduction implementation.

--- c64convert.py
+++ c64convert.py
@@ -34,7 +34,7 @@ def main(args):
     target_shape = (200, 320, 3)
     if image.shape != target_shape:
         # Resize image if necessary
-        image = skimage.transform.rescale(image, target_shape)
+        image = skimage.transform.resize(image, target_shape, order=3)
 
     # Convert sRGB image to Lab image
     image_lab = skimage.color.rgb2lab(image)
@@ -45,42 +45,39 @@ def main(args):
     palette_lab /= palette_lab[:, 0].max() / 100.0
 
     # For each pixel find the palette color that minimizes delta-E
-    # TODO: Don't hardcode array size?
     differences = np.zeros((16, *target_shape[:2]))
     for color_index, palette_color in enumerate(palette_lab):
         differences[color_index, :, :] = skimage.color.deltaE_cie76(image_lab, palette_color)
     best_indices = differences.argmin(axis=0)
 
-    """
-    best_image = convert_index_image_to_color_image(best_indices, srgb_palette)
-    plt.imshow(best_image, interpolation="nearest")
-    plt.show()
-    """
-
     # Divide image into 8x8 pixel blocks and make sure each block contains only two colors
     split_indices = blockshaped(best_indices, 8, 8)
-    reduced_indices = np.zeros(split_indices.shape)
+    reduced_indices = split_indices.copy()
     for i, block in enumerate(split_indices):
         unique_values, value_counts = np.unique(block, return_counts=True)
         if unique_values.shape[0] > 2:
             # More than two colors per block, must reduce!
+            # Sort color indices in descending order based on counts
             unique_values_sorted_by_count = unique_values[np.argsort(value_counts)][::-1]
-            print(unique_values_sorted_by_count)
-            print(np.sort(value_counts)[::-1])
-            print()
 
-            first_color_idx = unique_values_sorted_by_count[0]
-            second_color_idx = unique_values_sorted_by_count[1]
+            # Get Lab coordinates for each index to be reduced and
+            # compare to most common and second most common color to see which index to use
+            top_indices = unique_values_sorted_by_count[:2]
+            differences = np.zeros((2, unique_values_sorted_by_count.shape[0] - 2))
+            for top_index in range(0, 2):
+                differences[top_index, :] = skimage.color.deltaE_cie76(palette_lab[unique_values_sorted_by_count[2:]],
+                                                                       palette_lab[top_indices[top_index]])
 
-            # Get Lab coordinates for each index to be reduced
-            # Compare to most common and second most common color to see which index to use
+            replacement_indices = top_indices[np.argmin(differences, axis=0)]
 
-            # TODO: Don't pass values through
-            reduced_indices[i, :, :] = block
-            pass
-        else:
-            # Pass block through without modification
-            reduced_indices[i, :, :] = block
+            # Create block with reduced colors
+            for index_in_array, color_to_reduce in enumerate(unique_values_sorted_by_count[2:]):
+                reduced_indices[i, block == color_to_reduce] = replacement_indices[index_in_array]
+
+    reduced_indices = unblockshaped(reduced_indices, *(target_shape[:2]))
+    reduced_image = convert_index_image_to_color_image(reduced_indices, srgb_palette)
+    plt.imshow(reduced_image, interpolation="nearest")
+    plt.show()
 
 
 def convert_index_image_to_color_image(indices, palette):
@@ -103,9 +100,9 @@ def blockshaped(arr, nrows, ncols):
     each subblock preserving the "physical" layout of arr.
     """
     h, w = arr.shape
-    return (arr.reshape(h//nrows, nrows, -1, ncols)
-               .swapaxes(1,2)
-               .reshape(-1, nrows, ncols))
+    return (arr.reshape(h // nrows, nrows, -1, ncols)
+            .swapaxes(1, 2)
+            .reshape(-1, nrows, ncols))
 
 
 def unblockshaped(arr, h, w):
@@ -117,9 +114,9 @@ def unblockshaped(arr, h, w):
     then the returned array preserves the "physical" layout of the sublocks.
     """
     n, nrows, ncols = arr.shape
-    return (arr.reshape(h//nrows, -1, nrows, ncols)
-               .swapaxes(1,2)
-               .reshape(h, w))
+    return (arr.reshape(h // nrows, -1, nrows, ncols)
+            .swapaxes(1, 2)
+            .reshape(h, w))
 
 
 if __name__ == '__main__':
