4d0ad7fac15b239ab48f6254d58b891d5f0fa20f
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2016-07-20 15:29:06 +0300

    Start reducing colors in each block to 2.

--- c64convert.py
+++ c64convert.py
@@ -45,25 +45,81 @@ def main(args):
     palette_lab /= palette_lab[:, 0].max() / 100.0
 
     # For each pixel find the palette color that minimizes delta-E
-    differences = np.zeros((16, 200, 320))
+    # TODO: Don't hardcode array size?
+    differences = np.zeros((16, *target_shape[:2]))
     for color_index, palette_color in enumerate(palette_lab):
         differences[color_index, :, :] = skimage.color.deltaE_cie76(image_lab, palette_color)
     best_indices = differences.argmin(axis=0)
 
-    best_match_image = np.zeros(target_shape)
-    for i in range(0, srgb_palette.shape[0]):
-        best_match_image[best_indices == i] = srgb_palette[i, :]
-
-    best_match_image /= best_match_image.max()
-    plt.imshow(best_match_image, interpolation="nearest")
+    """
+    best_image = convert_index_image_to_color_image(best_indices, srgb_palette)
+    plt.imshow(best_image, interpolation="nearest")
     plt.show()
+    """
 
-    # TODO: Divide image into 8x8 pixel blocks and make sure each block contains only two colors
+    # Divide image into 8x8 pixel blocks and make sure each block contains only two colors
+    split_indices = blockshaped(best_indices, 8, 8)
+    reduced_indices = np.zeros(split_indices.shape)
+    for i, block in enumerate(split_indices):
+        unique_values, value_counts = np.unique(block, return_counts=True)
+        if unique_values.shape[0] > 2:
+            # More than two colors per block, must reduce!
+            unique_values_sorted_by_count = unique_values[np.argsort(value_counts)][::-1]
+            print(unique_values_sorted_by_count)
+            print(np.sort(value_counts)[::-1])
+            print()
+
+            first_color_idx = unique_values_sorted_by_count[0]
+            second_color_idx = unique_values_sorted_by_count[1]
+
+            # Get Lab coordinates for each index to be reduced
+            # Compare to most common and second most common color to see which index to use
+
+            # TODO: Don't pass values through
+            reduced_indices[i, :, :] = block
+            pass
+        else:
+            # Pass block through without modification
+            reduced_indices[i, :, :] = block
 
 
-def find_best_palette_index(lab_pixel, lab_palette):
-    differences = np.array([skimage.color.deltaE_cie76(lab_pixel, color) for color in lab_palette])
-    return differences.argmin()
+def convert_index_image_to_color_image(indices, palette):
+    target_shape = (*indices.shape, 3)
+    image = np.zeros(target_shape)
+    for i in range(0, palette.shape[0]):
+        image[indices == i] = srgb_palette[i, :]
+
+    image /= image.max()
+    return image
+
+
+# blockshaped and unblockshaped courtesy of unutbu: http://stackoverflow.com/a/16873755/4350204
+def blockshaped(arr, nrows, ncols):
+    """
+    Return an array of shape (n, nrows, ncols) where
+    n * nrows * ncols = arr.size
+
+    If arr is a 2D array, the returned array should look like n subblocks with
+    each subblock preserving the "physical" layout of arr.
+    """
+    h, w = arr.shape
+    return (arr.reshape(h//nrows, nrows, -1, ncols)
+               .swapaxes(1,2)
+               .reshape(-1, nrows, ncols))
+
+
+def unblockshaped(arr, h, w):
+    """
+    Return an array of shape (h, w) where
+    h * w = arr.size
+
+    If arr is of shape (n, nrows, ncols), n sublocks of shape (nrows, ncols),
+    then the returned array preserves the "physical" layout of the sublocks.
+    """
+    n, nrows, ncols = arr.shape
+    return (arr.reshape(h//nrows, -1, nrows, ncols)
+               .swapaxes(1,2)
+               .reshape(h, w))
 
 
 if __name__ == '__main__':
